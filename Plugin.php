dac7807e02c9913d5e374a42848f71a8d1f4fba6
amoydavid <liuw@liuw.net>
2018-02-01 21:20:00 +0800

    init version

---
+++ Plugin.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the PowerWeChatComposer.
+ *
+ * (c) amoydavid <liuw@liuw.net>
+ * (c) mingyoung <mingyoungcheung@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PowerWeChatComposer;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    protected $activated = true;
+
+    /**
+     * Apply plugin modifications to Composer.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
+     * Listen events.
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            PackageEvents::PRE_PACKAGE_UNINSTALL => 'prePackageUninstall',
+            ScriptEvents::POST_AUTOLOAD_DUMP => 'postAutoloadDump',
+        ];
+    }
+
+    /**
+     * @param \Composer\Installer\PackageEvent
+     */
+    public function prePackageUninstall(PackageEvent $event)
+    {
+        $this->activated = false;
+    }
+
+    /**
+     * @param \Composer\Script\Event $event
+     */
+    public function postAutoloadDump(Event $event)
+    {
+        if (!$this->activated) {
+            return;
+        }
+        $vendorPath = rtrim($event->getComposer()->getConfig()->get('vendor-dir'), '/');
+        $manifest = new ManifestManager(
+            $vendorPath, $vendorPath.'/powerwechat-composer/powerwechat-composer/extensions.php'
+        );
+
+        $manifest->unlink()->build();
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+<p align="center">
+    <h1 align="center">PowerWeChat Composer Plugin</h1>
+</p>
+
+
+This is the composer plugin for [PowerWeChat](https://github.com/amoydavid/PowerWeChat).
+
+
+Usage
+---
+
+Set the `type` to be `powerwechat-extension` in your package composer.json file:
+
+```json
+{
+    "name": "your/package",
+    "type": "powerwechat-extension"
+}
+```
+
+Specify server observer classes in the extra section:
+
+```json
+{
+    "name": "your/package",
+    "type": "powerwechat-extension",
+    "extra": {
+        "observers": [
+            "Acme\\Observers\\Handler"
+        ]
+    }
+}
+```
+
